6e86d3a9ade5256ab2633458992acb5da6e29d75
sidlayi <bonginkosinkosi290@gmail.com>
2023-03-20 08:19:40 -0700

    0-putchar.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+  *main - Entry point
+  *Description: 'print alphabets lowercase'
+  *Return: Always 0
+  */
+int main(void)
+{
+	putcher(65);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'Print alphabets lowercase'
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putcher(n);
+		n++;
+	}
+	putcher('\n')
+	return (0);
+}

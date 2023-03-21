63aa8cbf3ee1341383be4b79ec41a46f45931978
sidlayi <bonginkosinkosi290@gmail.com>
2023-03-21 14:11:35 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*add - adds two values
+*@n1: The frist number to be computed
+*@n1: The second number to be computed
+*Return: integer value
+*/
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}

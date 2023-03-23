1ac4d72746b1e6e630a3906f426e8f413e6043c0
sidlayi <bonginkosinkosi290@gmail.com>
2023-03-23 08:02:02 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,42 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
 {
-	int bytes, i;
-	char *arr;
+        int i;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+        printf("Infinite loop incoming :(\n");
 
-	bytes = atoi(argv[1]);
+        i = 0;
 
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
+        while (i < 10)
+        {
+                putchar(i);
+        }
 
-	arr = (char *)main;
+        printf("Infinite loop avoided! \\o/\n");
 
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
-	}
-	return (0);
+        return (0);
 }

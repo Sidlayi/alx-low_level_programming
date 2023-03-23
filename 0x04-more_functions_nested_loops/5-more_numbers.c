1f92ad9489c7b38d9cba971425d54ee3e5dbd839
sidlayi <bonginkosinkosi290@gmail.com>
2023-03-23 06:42:12 -0700

    vi 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int c, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+				_putchar('1');
+			_putchar(c % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

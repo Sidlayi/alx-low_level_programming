c0eba0c26a69ab7eb444054f73023d00a84f8a92
sidlayi <bonginkosinkosi290@gmail.com>
2023-03-29 17:28:04 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,27 +1,27 @@
 #include "main.h"
+
 /**
- * print_number - prints an integer.
- * @n:.integer to be printed.
- *
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
+
 void print_number(int n)
 {
-	unsigned int i = n;
+        unsigned int n1;
 
-	if (n < 0)
-	{
-		/**
-		 * checks if the input number n is negative.
-		 *  If it is, the function prints a - character
-		 *  and then makes n positive by multiplying it by -1.
-		 */
+        if (n < 0)
+        {
+                n1 = -n;
+                _putchar('-');
+        } else
+        {
+                n1 = n;
+        }
 
-		_putchar('-');
-		i = -i;
-	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+        if (n1 / 10)
+        {
+                print_number(n1 / 10);
+        }
+
+        _putchar((n1 % 10) + '0');
 }

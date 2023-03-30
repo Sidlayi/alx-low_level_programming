3872fafbfde386fd6932a7bd05523441add8f760
sidlayi <bonginkosinkosi290@gmail.com>
2023-03-29 18:23:02 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MIN_H
+#define MIN_H
+
+#include <stdio.h>
+
+int _putcher(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);

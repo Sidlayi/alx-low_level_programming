0ad0ac3b8f51faf00b19fa865460c7fac9ce21e3
sidlayi <bonginkosinkosi290@gmail.com>
2023-04-07 08:32:10 -0700

    task5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,9 +1,35 @@
+#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-
- 
-int main(void)
+/**
+ * main - adds two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
-   puts("\"Programming is like building a multilingual puzzle");	
-    return (0);
+	int i, j, add = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+	}
+	else if (argc == 1)
+		printf("0\n");
+
+	return (0);
 }

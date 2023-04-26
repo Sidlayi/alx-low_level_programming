01488e92cb6e3104aa9f9984452a8338f3672983
sidlayi <bonginkosinkosi290@gmail>
2023-04-26 15:56:22 -0700

    task1

--- 0-strcat.c
+++ 0-strcat.c
@@ -1,27 +1,52 @@
 #include "main.h"
 
 /**
- *_strcat - concatinate 2 strings.
- *@dest: first string.
- *@src: second string.
- *Return: string.
+ * _strcat - function that concatenates two strings
+ *
+ * Description: function that concatenates two strings
+ *
+ * @dest: String to place at the start
+ * @src: String to place at the end
+ *
+ * Return: Pointer to concatenated string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int index_src, index_dest, length_src, length_dest, src_end, dest_end;
 
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
+	src_end = dest_end = length_src = length_dest = 0;
+	index_src = index_dest = 0;
+	while (1)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		if (src_end == 1 && dest_end == 1)
+			break;
+
+		if (*(src + index_src) == '\0')
+		{
+			src_end = 1;
+		}
+		else
+		{
+			length_src++;
+			index_src++;
+		}
+
+		if (*(dest + index_dest) == '\0')
+		{
+			dest_end = 1;
+		}
+		else
+		{
+			length_dest++;
+			index_dest++;
+		}
 	}
 
-	dest[i] = '\0';
+	for (index_src = 0; index_src < length_src; index_src++)
+	{
+		*(dest + (index_dest + index_src)) = *(src + index_src);
+	}
 
 	return (dest);
 }
